53f368bacf39fef9f9f0e096cefedc07b4c505b9
SpaceBuckett <shehxadv2.0@gmail.com>
2021-11-16 20:43:31 +0500

    Launch 1-0-1, Introducing Basic Widgets

---
+++ basicwidgets/lib/UI/card1.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/fooderlich_theme.dart';
+import 'package:fooderlich/main.dart';
+
+class Card1 extends StatelessWidget {
+  const Card1({Key? key}) : super(key: key);
+  // 1
+  final String category = 'Editor\'s Choice';
+  final String title = 'The Art of Dough';
+  final String description = 'Learn to make the perfect bread.';
+  final String chef = 'Ray Wenderlich';
+
+  // 2
+  @override
+  Widget build(BuildContext context) {
+    // 3
+    return Center(
+      // TODO: Card1 Decorate Container
+      child: Container(
+        child: Stack(
+          children: [
+            Text(
+              category,
+              style: FooderlichTheme.darkTextTheme.bodyText1,
+            ),
+            Positioned(
+              child: Text(
+                title,
+                style: FooderlichTheme.darkTextTheme.headline5,
+              ),
+              top: 20,
+            ),
+            Positioned(
+              child: Text(
+                description,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 30,
+              right: 0,
+            ),
+            Positioned(
+              child: Text(
+                chef,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 10,
+              right: 0,
+            ),
+          ],
+        ),
+        padding: EdgeInsets.all(16),
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+        ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/mag1.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ basicwidgets/lib/UI/home.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/UI/card1.dart';
+import 'package:fooderlich/UI/card2.dart';
+import 'package:fooderlich/UI/card3.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  static List<Widget> pages = <Widget>[
+    const Card1(),
+    const Card2(),
+    const Card3(),
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Fooderlich',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      body: pages[_selectedIndex],
+      // Center(
+      //   child: Text(
+      //     'Let\'s get cooking 👩‍🍳',
+      //     style: Theme.of(context).textTheme.headline1,
+      //   ),
+      // ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: [
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card2',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card3',
+          ),
+        ],
+      ),
+    );
+  }
+}
