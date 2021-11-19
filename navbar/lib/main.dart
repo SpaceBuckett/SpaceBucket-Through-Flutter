68ef53b401d6b41c35dba203534a44360383c26b
SpaceBuckett <shehxadv2.0@gmail.com>
2021-11-19 09:52:11 +0500

    The Nav Bar

---
+++ navbar/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/UI/home.dart';
+import 'fooderlich_theme.dart';
+
+void main() {
+  // 1
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  // 2
+  const Fooderlich({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.dark();
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      title: 'Fooderlich',
+      // 4
+      home: Home(),
+    );
+  }
+}
