2dfc1ea6c1028086359e191ee31bdca87a576e10
SpaceBuckett <shehxadv2.0@gmail.com>
2021-11-26 21:06:33 +0500

    The Flutter Scrollable Widgets in Action

---
+++ ScrollableWidgets/starter/lib/screens/explore_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/components/toda_recipie_listview.dart';
+
+import '../components/components.dart';
+import '../models/models.dart';
+import '../api/mock_fooderlich_service.dart';
+
+class ExploreScreen extends StatelessWidget {
+  // 1
+  final mockService = MockFooderlichService();
+
+  ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // 2
+    // 1
+    return FutureBuilder(
+      // 2
+      future: mockService.getExploreData(),
+      // 3
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+        // TODO: Add Nested List Views
+        // 4
+        if (snapshot.connectionState == ConnectionState.done) {
+          // 5
+          final recipes = snapshot.data?.todayRecipes ?? [];
+          // TODO: Replace this with TodayRecipeListView
+          return TodayRecipeListView(recipes: recipes);
+        } else {
+          // 6
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+}
